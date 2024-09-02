c20f929fdb9a10450804acc975958880425dde63
JavierAMartinez <soportepartner59@gmail.com>
2024-09-02 11:48:59 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# p1_donut_app_javier_martinez
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/p1_donut_app_javier_martinez/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.p1_donut_app_javier_martinez
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
