b80b2878a9ac1430a6c3125c0c20407b2f761768
JavierAMartinez <soportepartner59@gmail.com>
2024-09-09 13:32:46 -0600

    Tab bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:p1_donut_app_javier_martinez/utils/MyTab.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,15 +9,64 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Widget> myTabs = [
+    //donut tab
+    const MyTab(iconPath: 'lib/icons/donut.png'),
+    //burguer tab
+    const MyTab(iconPath: 'lib/icons/donut.png'),
+    //smothie tab
+    const MyTab(iconPath: 'lib/icons/donut.png'),
+    //pancake tab
+    const MyTab(iconPath: 'lib/icons/donut.png'),
+    //pizza tab
+    const MyTab(iconPath: 'lib/icons/donut.png'),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          backgroundColor: Colors.blue,
-          leading: const Icon(
-            Icons.menu,
-            color: Colors.white,
-          )),
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Icon(
+              Icons.menu,
+              color: Colors.grey[800],
+            ),
+          ),
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 24.0),
+              child: Icon(Icons.person),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            //Texto: I want to eat
+            const Padding(
+              padding: EdgeInsets.all(24.0),
+              child: Row(
+                children: [
+                  Text("I want to ", style: TextStyle(fontSize: 24)),
+                  Text("Eat",
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline)),
+                ],
+              ),
+            ),
+            //Tab bar
+            TabBar(tabs: myTabs)
+
+            //Tab bar view
+
+            //Total del carrito
+          ],
+        ),
+      ),
     );
   }
 }
