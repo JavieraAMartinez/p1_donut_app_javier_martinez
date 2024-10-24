95af11fb97dfc0f50504e74f9d640ba0a486a5f6
JavierAMartinez <soportepartner59@gmail.com>
2024-10-24 09:45:50 -0600

    view card final added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -29,65 +29,112 @@ class _HomePageState extends State<HomePage> {
     //smothie tab
     const MyTab(
       iconPath: 'lib/icons/pizza.png',
-      label: "Smothie",
+      label: "Pizza",
     ),
     //pizza tab
     const MyTab(
       iconPath: 'lib/icons/smoothie.png',
-      label: "Pizza",
+      label: "Smothie",
     ),
+    const MyTab(iconPath: 'lib/icons/pancakes.png', label: "Pancakes")
   ];
+
+  //state
+  Map<String, int> cartItems = {};
+  double totalAmount = 0;
+
+  //add
+  void addToCart(String itemName, double itemPrice) {
+    setState(() {
+      if (cartItems.containsKey(itemName)) {
+        cartItems[itemName] = cartItems[itemName]! + 1;
+      } else {
+        cartItems[itemName] = 1;
+      }
+      totalAmount += itemPrice;
+    });
+  }
+
+  //delete
+  void removeFromCart(String itemName, double itemPrice) {
+    setState(() {
+      if (cartItems.containsKey(itemName)) {
+        if (cartItems[itemName] == 1) {
+          cartItems.remove(itemName);
+        } else {
+          cartItems[itemName] = cartItems[itemName]! - 1;
+        }
+        totalAmount -= itemPrice;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: myTabs.length,
       child: Scaffold(
         appBar: AppBar(
+          toolbarHeight: 60,
           backgroundColor: Colors.transparent,
+          elevation: 0,
           leading: Padding(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: Icon(
-              Icons.menu,
-              color: Colors.grey[800],
-            ),
+            padding: const EdgeInsets.only(left: 16.0),
+            child: Icon(Icons.menu, color: Colors.grey[800]),
           ),
           actions: const [
             Padding(
-              padding: EdgeInsets.only(right: 24.0),
-              child: Icon(Icons.person),
-            )
+              padding: EdgeInsets.only(right: 16.0),
+              child: Icon(Icons.person, color: Colors.grey),
+            ),
           ],
         ),
         body: Column(
           children: [
-            //Texto: I want to eat
             const Padding(
-              padding: EdgeInsets.all(24.0),
+              padding: EdgeInsets.all(16.0),
               child: Row(
                 children: [
                   Text("I want to ", style: TextStyle(fontSize: 24)),
-                  Text("Eat",
-                      style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          decoration: TextDecoration.underline)),
+                  Text(
+                    "Eat",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
                 ],
               ),
             ),
-            //Tab bar
-            TabBar(tabs: myTabs),
-
-            //Tab bar view
-            const Expanded(
-                child: TabBarView(children: [
-              DonutTab(),
-              BurguerTab(),
-              SmothieTab(),
-              PancakeTab(),
-              PizzaTab(),
-            ]))
-
-            //Total del carrito
+            Center(
+              child: TabBar(
+                tabs: myTabs,
+                isScrollable: true,
+                indicatorColor: Colors.pink,
+              ),
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  DonutTab(
+                      addToCart: addToCart, removeFromCart: removeFromCart),
+                  BurgerTab(
+                      addToCart: addToCart, removeFromCart: removeFromCart),
+                  SmoothieTab(
+                      addToCart: addToCart, removeFromCart: removeFromCart),
+                  PanCakeTab(
+                      addToCart: addToCart, removeFromCart: removeFromCart),
+                  PizzaTab(
+                      addToCart: addToCart, removeFromCart: removeFromCart),
+                ],
+              ),
+            ),
+            CartBar(
+              itemCount: cartItems.values.fold(0, (a, b) => a + b),
+              totalAmount: totalAmount,
+              onViewCartPressed: () {},
+            ),
           ],
         ),
       ),
